f755816822818ca0587c41efd67a5e8c9905f434
winstonbarbe <winston.barbe@gmail.com>
2020-10-26 20:12:17 -0400

    Adds authentication

--- src/router/index.js
+++ src/router/index.js
@@ -1,24 +1,40 @@
-import Vue from 'vue'
-import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
+import Vue from "vue";
+import VueRouter from "vue-router";
+import Home from "../views/Home.vue";
+import About from "../views/About.vue";
+import Signup from "../views/Signup.vue";
+import Login from "../views/Login.vue";
+import Logout from "../views/Logout.vue";
 
-Vue.use(VueRouter)
+Vue.use(VueRouter);
 
 const routes = [
-  {
-    path: '/',
-    name: 'Home',
-    component: Home
+  { 
+    path: "/", 
+    name: "home", 
+    component: Home 
   },
-  {
-    path: '/about',
-    name: 'About',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
-  }
-]
+  { 
+    path: "/about", 
+    name: "about", 
+    component: About 
+  },
+  { 
+    path: "/signup", 
+    name: "signup", 
+    component: Signup 
+  },
+  { 
+    path: "/login", 
+    name: "login", 
+    component: Login 
+  },
+  { 
+    path: "/logout", 
+    name: "logout", 
+    component: Logout }
+];
+
 
 const router = new VueRouter({
   mode: 'history',
