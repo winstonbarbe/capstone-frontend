acacdd3af5c63579314c221cff0c6f4747768ca9
winstonbarbe <winston.barbe@gmail.com>
2020-11-18 14:21:31 -0500

    adds styling to matches show

--- src/views/MatchesIndex.vue
+++ src/views/MatchesIndex.vue
@@ -2,6 +2,170 @@
   <div class="matches-index">
     <!-- Mutual Match Data  -->
     <!-- Matches should be sorted by either updated at or Last message  -->
+    <div class="container p-0">
+      <!-- Compatibles filter -->
+      <ul
+        class="portfolio-filter list-inline text-center filter-bg bg-secondary"
+      >
+        <!-- Minimum Age Filter -->
+        <li class="nav-item dropdown">
+          <a aria-haspopup="true" aria-expanded="false">MINUMUM AGE</a>
+          <div
+            class="dropdown-menu animated fadeIn fast"
+            aria-labelledby="dropdown1"
+          >
+            <div class="row" style="text-align:center;">
+              <div class="col-md-12 sub-menu">
+                <form method="dialog">
+                  <label>SET</label>
+                  <select class="form-control" v-model="minAge">
+                    <option v-for="age in ages">{{ age }}</option>
+                  </select>
+                  <br />
+                </form>
+              </div>
+              <!-- / sub-menu -->
+            </div>
+            <!-- / row -->
+          </div>
+          <!-- / dropdown-menu -->
+        </li>
+        <!-- Max Age Filter -->
+        <li class="nav-item dropdown">
+          <a aria-haspopup="true" aria-expanded="false">MAXIMUM AGE</a>
+          <div
+            class="dropdown-menu animated fadeIn fast"
+            aria-labelledby="dropdown1"
+          >
+            <div class="row" style="text-align:center;">
+              <div class="col-md-12 sub-menu">
+                <form method="dialog">
+                  <label>SET</label>
+                  <select class="form-control" v-model="maxAge">
+                    <option v-for="age in ages">{{ age }}</option>
+                  </select>
+                  <br />
+                </form>
+              </div>
+              <!-- / sub-menu -->
+            </div>
+            <!-- / row -->
+          </div>
+          <!-- / dropdown-menu -->
+        </li>
+        <!-- Max Distance -->
+        <!-- Minimum Age Filter -->
+        <li class="nav-item dropdown">
+          <a aria-haspopup="true" aria-expanded="false">Range</a>
+          <div
+            class="dropdown-menu animated fadeIn fast"
+            aria-labelledby="dropdown1"
+          >
+            <div class="row" style="text-align:center;">
+              <div class="col-md-12 sub-menu">
+                <form method="dialog">
+                  <label>SET</label>
+                  <select class="form-control" v-model="maxDistance">
+                    <option v-for="mile in miles">{{ mile }}</option>
+                  </select>
+                  <br />
+                </form>
+              </div>
+              <!-- / sub-menu -->
+            </div>
+            <!-- / row -->
+          </div>
+          <!-- / dropdown-menu -->
+        </li>
+        <!-- / dropdown -->
+      </ul>
+      <!-- / Compatibles filter -->
+    </div>
+    <div id="page-content-wrapper">
+      <section id="portfolio" class="col3">
+        <div class="container">
+          <h1 class="section-title">Matches</h1>
+
+          <div class="row">
+            <div v-for="match in mutualMatches" class="col-lg-4">
+              <div class="blog block">
+                <div class="post-image">
+                  <router-link :to="`/matches/${match.id}`">
+                    <div class="hovereffect">
+                      <img
+                        class="img-responsive"
+                        :src="match.matcher.image_url"
+                        :alt="match.matcher.name"
+                      />
+                      <div class="overlay">
+                        <div class="hovereffect-title">
+                          <!-- style for a tags aren't working for router links -->
+
+                          <a>
+                            <h5 class="project-title">
+                              {{ match.matcher.name }}
+                            </h5>
+                            <p
+                              v-if="match.matcher.ranking > 7"
+                              class="project-skill"
+                            >
+                              Super Compatible
+                            </p>
+                            <p v-else class="project-skill">
+                              {{ match.matcher.sun_sign }} -
+                              {{ match.matcher.moon_sign }} -
+                              {{ match.matcher.ascending_sign }}
+                            </p> </a
+                          ><!-- / project link -->
+                        </div>
+                        <!-- / hovereffect-title -->
+                      </div>
+                      <!-- / overlay -->
+                    </div>
+                  </router-link>
+                </div>
+                <!-- / post-image -->
+                <div class="post-content bg-secondary">
+                  <h4 class="post-title">
+                    {{ match.matcher.name }}
+                    <a>{{ $parent.age(match.matcher.birth_date) }}</a
+                    ><br />
+                    <a
+                      ><i>({{ match.matcher.pronouns }})</i> -
+                      {{ match.matcher.distance }} Miles away
+                    </a>
+                  </h4>
+                  <p class="mb-3">
+                    {{ match.matcher.bio }}
+                  </p>
+                  <h6 v-if="match.last_message" class="post-title">
+                    <a class="text-primary">Last Message</a><br />
+                    <a class="text-sm">{{ match.last_message.body }}</a
+                    ><br />
+                    <a class="text-xs"
+                      >{{ match.last_message.name }} -
+                      {{
+                        $parent.lastMessageSent(match.last_message.created_at)
+                      }}
+                      <i>hours ago</i><br
+                    /></a>
+                  </h6>
+                  <button type="button" class="btn btn-outline-danger">
+                    UNMATCH
+                  </button>
+                </div>
+                <!-- / post-content -->
+              </div>
+              <!-- / blog-block -->
+            </div>
+            <!-- / column -->
+          </div>
+          <!-- / row -->
+          <!-- / portfolio -->
+        </div>
+        <!-- / container -->
+      </section>
+    </div>
     <h1>Mutual Matches</h1>
 
     <p v-if="mutualMatches.length === 0">No matches yet</p>
